660a8f0838f82fa86176baae3c9eb74e0f4cb632
Roshni1103 <pillairoshni1103@gmail.com>
2024-05-29 23:58:32 +0530

    Add files via upload

---
+++ VotingEligibility.java
@@ -0,0 +1,13 @@
+public class VotingEligibility {
+    public static void main(String[] args) {
+        // Define the age of the person
+        int age = 20;
+
+        // Check if the person is eligible to vote
+        if (age >= 18) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+    }
+}
